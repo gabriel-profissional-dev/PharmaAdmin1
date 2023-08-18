06ce1f21fb830251eff6b897c741c25ead740dc7
gabriel-profissional-dev <gabriel.profissional.dev@gmail.com>
2023-08-18 10:56:38 -0300

    comit 1 - Iniciando o Projeto PharmaAdmin

---
+++ PharmaTech.PharmaAdmin.Domain/Enum/TipoMedicamento.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PharmaTech.PharmaAdmin.Domain.Enum
+{
+    public enum TipoMedicamento
+    {
+        Analgesico,
+        Antibiotico,
+        AntiInflamatorio,
+        Antialergico,
+        Antidepressivo,
+        Diuretico,
+        Antipiretico,
+        Sedativo,
+        Anticoagulante,
+        Hormonal    
+    }
+}
---
+++ PharmaTech.PharmaAdmin/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using PharmaTech.PharmaAdmin.Domain;
+
+namespace PharmaTech.PharmaAdmin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
